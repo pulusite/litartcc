c132ef0665930e63793d0d78675fc9ea11975d0b
zhangdong8 <zhangdong8@jd.com>
2017-11-15 16:56:32 +0800

    update

---
+++ src/cache/RedisTest.java
@@ -0,0 +1,7 @@
+package cache;
+
+/**
+ * Created by zhangdong on 2017/11/15.
+ */
+public class RedisTest {
+}
---
+++ src/mq/ActiveMqTest.java
@@ -0,0 +1,7 @@
+package mq;
+
+/**
+ * Created by zhangdong on 2017/11/15.
+ */
+public class ActiveMqTest {
+}
