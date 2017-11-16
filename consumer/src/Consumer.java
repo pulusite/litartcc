4e510609eb7a6398537079242969f8b127147654
zhangdong8 <zhangdong8@jd.com>
2017-11-16 16:45:59 +0800

    update

---
+++ consumer/src/Consumer.java
@@ -0,0 +1,14 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by zhangdong on 2017/11/16.
+ */
+public class Consumer {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("file:F:/github/litartcc/src/resources/dubbo-consumer.xml");
+        context.start();
+        DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello("world"); // 执行远程方法
+        System.out.println(hello);
+    }
+}
---
+++ consumer/src/DemoService.java
@@ -0,0 +1,6 @@
+/**
+ * Created by zhangdong on 2017/11/16.
+ */
+public interface DemoService {
+    String sayHello(String name);
+}
