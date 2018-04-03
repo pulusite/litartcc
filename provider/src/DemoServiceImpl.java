7519da594e43a72d826e306a219a81f4dec37e10
zhangdong8 <zhangdong8@jd.com>
2018-04-03 11:30:29 +0800

    update

--- provider/src/DemoServiceImpl.java
+++ provider/src/DemoServiceImpl.java
@@ -1,7 +1,7 @@
 /**
  * Created by zhangdong on 2017/11/16.
  */
-public class DemoServiceImpl implements DemoService{
+public class DemoServiceImpl implements DemoService {
     public String sayHello(String name) {
         return "Hello Dubbo,Hello " + name;
     }
