36b91e0188edd9ad77d578da8cb26d1a95a9c80c
zhangdong8 <zhangdong8@jd.com>
2018-04-08 14:08:02 +0800

    update

--- src/spring/proxy/UserService.java
+++ src/spring/proxy/UserService.java
@@ -4,5 +4,5 @@ package spring.proxy;
  * Created by dongzhang on 5/13/17.
  */
 public interface UserService {
-    abstract void add();
+    void add();
 }
--- src/spring/proxy/UserServiceImpl.java
+++ src/spring/proxy/UserServiceImpl.java
@@ -4,7 +4,6 @@ package spring.proxy;
  * Created by dongzhang on 5/13/17.
  */
 public class UserServiceImpl implements UserService {
-    @Override
     public void add() {
         System.out.println("------add------");
     }
