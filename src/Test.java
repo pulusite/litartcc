8cd1a79f3796f9f94390a06fca5b0d55233a8296
zhangdong8 <zhangdong8@jd.com>
2018-05-23 14:41:25 +0800

    junit test

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,9 @@ public class Test {
         System.out.println("MYgGnQE2+DAS973vd1DFHg==".length());
         System.out.println("ubuntu is good");
     }
+
+    @org.junit.Test
+    public void junitTest(){
+        System.out.println("junit test");
+    }
 }
